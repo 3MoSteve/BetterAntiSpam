762ab27fc75404b3068c634af88be73b240a3e46
3MoSteve <45514708+3MoSteve@users.noreply.github.com>
2022-02-22 02:43:45 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    TOKEN: "توكن البوت",
+    PREFIX: "$",
+    domain: "http://127.0.0.1",
+    GOOGLE_RECAPTCHA_SITE_KEY: "Google recaptcha sitekey ! video coming soon!",
+    GOOGLE_RECAPTCHA_SECRET_KEY: "Google recaptcha secretkey ! video coming soon!"
+
+}
